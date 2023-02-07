3540d32635762132a32643fa86dba112fd9e7776
Paez11 <victorpaez117@gmail.com>
2023-02-07 10:56:57 +0100

    client service, mock cashiers

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IClient } from '../model/IClient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  private url:string = 'http://localhost:8080/api/client';
+
+  constructor(private http:HttpClient) { }
+
+  getAll():Observable<IClient[]>{
+    return this.http.get<IClient[]>(this.url);
+  }
+
+  get(id:number):Observable<IClient>{
+    return this.http.get<IClient>(this.url+'/'+id);
+  }
+
+  create(client:IClient):Observable<IClient>{
+    return this.http.post<IClient>(this.url, client);
+  }
+
+  update(client:IClient):Observable<IClient>{
+    return this.http.put<IClient>(this.url, client);
+  }
+
+  delete(id:number):Observable<IClient>{
+    return this.http.delete<IClient>(this.url+'/'+id);
+  }
+}
