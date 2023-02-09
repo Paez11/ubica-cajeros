8fee48867e7b4c530b346fc6cca80ab8b4c52bbe
JoseCP82 <josecamarap@gmail.com>
2023-02-09 10:07:10 +0100

    transaction services implemented

---
+++ src/app/services/transaction.service.ts
@@ -0,0 +1,32 @@
+ import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { DTOTransaction } from '../model/DTOTransaction';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+
+  //private url:string = "http://localhost:8080/api/transactions";
+
+  constructor(private http:HttpClient) { }
+
+  createTransaction(client, cashier, type, amount):Observable<any> {
+    if(!client || !cashier || !type || !amount || amount<0) {
+      throw new Error("Data error.");
+    }
+
+    let data:DTOTransaction = {
+      client:client,
+      cashier:cashier,
+      type:type,
+      amount:amount
+    }
+    const endpoint = environment.api.url+environment.api.endpoint.transactions;
+    return this.http.post(endpoint, data, {
+        headers: { 'Content-Type': 'application/json' }
+    });
+  }
+}
