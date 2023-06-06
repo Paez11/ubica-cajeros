6b2da5e2e640a30e3daeab5bb810443c1f8226b3
Juan Manuel <juan.gomezwals@atmira.com>
2023-06-06 14:35:36 +0200

    ConfirmModal standby

--- src/app/components/confirm-modal/confirm-modal.component.ts
+++ src/app/components/confirm-modal/confirm-modal.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
+declare var bootstrap: any;
 @Component({
   selector: 'app-confirm-modal',
   templateUrl: './confirm-modal.component.html',
-  styleUrls: ['./confirm-modal.component.scss']
+  styleUrls: ['./confirm-modal.component.scss'],
 })
-export class ConfirmModalComponent implements OnInit {
+export class ConfirmModalComponent implements OnInit, AfterViewInit {
+  _modalConfirm;
+  showModal = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this._modalConfirm = new bootstrap.Modal(
+      document.getElementById('modalConfirm'),
+      {}
+    );
   }
 
+  close() {
+    this._modalConfirm.hide();
+    this.showModal = false;
+  }
+  open() {
+    this._modalConfirm.show();
+    this.showModal = true;
+  }
 }
